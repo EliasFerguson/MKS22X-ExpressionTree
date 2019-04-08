4615130e674936cef9d222c3bfd83b1cf5c76690
Elias Ferguson <eferguson00@stuy.edu>
2019-04-08 11:57:52 -0400

    all answers in main are correct, need to write toString()

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -85,7 +85,7 @@ public class ExpressionTree{
   /*return the expression as an infix notation string with parenthesis*/
   /* The sample tree would be: "(3 + (2 * 10))"     */
   public String toString(){
-    /*you are to write this method*/
+    
     return "";
   }
 
